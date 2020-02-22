6d0a02dc4ba98fe0b60515cb7a755b6393cfd4b1
vijay pandey <vijaypandey@vijays-MacBook-Air.local>
2020-02-23 01:24:01 +0530

    Hello World Using a Bean

--- src/main/java/com/vijay/springboot/restapp/controller/HelloWorldController.java
+++ src/main/java/com/vijay/springboot/restapp/controller/HelloWorldController.java
@@ -3,11 +3,18 @@ package com.vijay.springboot.restapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vijay.springboot.restapp.model.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world")
 	public String sayHelloWorld() {
 		return "Hello-world";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean getHelloWorldBean() {
+		return new HelloWorldBean("Hello-World-Bean");
 	}
 }
---
+++ src/main/java/com/vijay/springboot/restapp/model/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.vijay.springboot.restapp.model;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+	
+}
