d1917ee690024789b7c80491cb640ec58fec8613
vijay pandey <vijaypandey@vijays-MacBook-Air.local>
2020-02-23 02:41:25 +0530

    Post Mappings for User Bean added

--- src/main/java/com/vijay/springboot/restapp/controller/UserController.java
+++ src/main/java/com/vijay/springboot/restapp/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.vijay.springboot.restapp.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vijay.springboot.restapp.model.User;
 import com.vijay.springboot.restapp.service.UserService;
@@ -15,16 +20,25 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping(path = "/users")
-	public List<User> retrieveAllUsers(){
+	public List<User> retrieveAllUsers() {
 		return userService.findAll();
 	}
-	
-	@GetMapping(path="/users/{id}")
+
+	@GetMapping(path = "/users/{id}")
 	public User retrieveUserById(@PathVariable int id) {
 		return userService.findOne(id);
 	}
-	
-	
+
+	@PostMapping(path = "/users")
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		User savedUser = userService.save(user);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getUserId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+
 }
